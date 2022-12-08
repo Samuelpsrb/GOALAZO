4054d53196ee8005fdaca3e7ae69f487b4f40067
Samuelpsrb <samuelanugrahpasaribu@gmail.com>
2022-12-08 09:57:00 +0700

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="about">
+      <h1 className="title">About</h1>
+      <div className="about-container">
+        <img src="/Samuel Miduk Anugrah Pasaribu.jpg" alt="Samuel pic" />
+        <p>Nama: Samuel Miduk Anugrah Pasaribu</p>
+        <p>NIM: 21120120130071</p>
+        <div className="about-app">
+          <h2 className="title">GOALAZO</h2>
+          <p>
+          </p>
+          <p>
+            GOALAZO merupakan aplikasi yang memberikan informasi mengenai sepakbola.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
